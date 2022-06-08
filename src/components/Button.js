a4b805951e2f1123b449c180355a87419e20955a
Musab Shaikh <musabs2802@gmail.com>
2022-06-08 20:14:39 +0530

    Initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Button = ({ title, color, onClick }) => {
+  return (
+    <button
+      className='btn'
+      style={{ backgroundColor: color }}
+      onClick={onClick}
+    >
+      {title}
+    </button>
+  )
+}
+
+Button.defualtProps = {
+  color: 'black',
+}
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+export default Button
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = () => {
+  const onClick = (e) => {
+    console.log(e)
+  }
+  
+  return (
+    <header className='header'>
+      <h1>Task Tracker</h1>
+      <Button title='Add' onClick={onClick} />
+    </header>
+  )
+}
+
+/** Default Props */
+// Header.defualtProps = {
+//     title: 'Task Tracker',
+// }
+
+/** Prop Types */
+// Header.propTypes = {
+//     title: PropTypes.string.isRequired,
+// }
+
+export default Header
